4a5214e4a90be38cb7cfed45eaecac336364329f
misschildhood <923219711@qq.com>
2019-09-13 00:52:27 +0800

    zuul网关基础工程

---
+++ xc-govern-gateway/src/main/java/com/xuecheng/govern/gateway/GatewayApplication.java
@@ -0,0 +1,19 @@
+package com.xuecheng.govern.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+/**
+ * @Description:
+ * @Author lyh-god
+ * @Date 2019/9/13
+ **/
+@SpringBootApplication
+@EnableZuulProxy//此工程是一个zuul网关
+public class GatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+}
